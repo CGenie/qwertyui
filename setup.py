561aad6ea1f4ae5e0aa5272d50ebe4843fb20ff4
Przemek Kaminski <cgenie@gmail.com>
2017-06-29 20:33:57 +0100

    1.0.1 version bumpup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages, setup
 
 setup(name='qwertyui',
-      version='1.0',
+      version='1.0.1',
       description='Some common Python functions and algorithms',
       author='Przemyslaw Kaminski',
       author_email='cgenie@gmail.com',
