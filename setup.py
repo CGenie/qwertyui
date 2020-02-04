028de7d5e8a5e940d4d7b9cb2f0f652d045fff03
Przemek Kaminski <pk@intrepidus.pl>
2020-02-04 07:41:35 +0100

    [setup] bumpup to 1.0.24

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 
 setup(name='qwertyui',
-      version='1.0.22',
+      version='1.0.24',
       description='Some common Python functions and algorithms',
       author='Przemyslaw Kaminski',
       author_email='cgenie@gmail.com',
